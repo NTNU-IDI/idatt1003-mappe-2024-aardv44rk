4ec1b0735fa956360dcd9c3aa2136100e7757725
Audun Andreas Langeland Aakermann <audunaakermann@gmail.com>
2024-10-29 17:00:45 +0100

    forbedret searchIngredient men ikke testet

--- foodwaste/src/test/java/FoodStorage.java
+++ foodwaste/src/test/java/FoodStorage.java
@@ -12,7 +12,7 @@ public class FoodStorage {
         ingredientList = new HashMap<>();
     }
 
-    public void addIngredient(Ingredient ingredient)
+    public void addIngredient(Ingredient ingredient, int amount)
     {
         // Sjekker om det finnes en ingrediens av samme navn i hashmappet allerede
         if (!ingredientList.containsKey(ingredient.getName())) 
@@ -20,27 +20,29 @@ public class FoodStorage {
         {
             ingredientList.put(ingredient.getName(), new ArrayList<>());       
         }
-        // Legger til ingrediensen 
+        // Legger til ingrediensen
         ingredientList.get(ingredient.getName()).add(ingredient);
     }
 
     public void searchIngredient(Ingredient ingredient)
     {
         {
+            int[] i = {1};
             String name = ingredient.getName();
-
-            // Dersom ingrediensen eksisterer i ingrediense                                                                                                                                                                                                                                                                                                                                                                                                                                                                       listen printer vi alle elementer i den tilhørende arraylisten
-            if (ingredientList.containsKey(name))
-            {
-                for (Ingredient food : ingredientList.get(name)) {
-                    System.out.println(food + "\n");
-                }
-            }
-            // replace med streams eller lambda om mulig
-            else
-            {
-                System.out.println(Collections.emptyList());
-            }
+            // Sjekker om navnet finnes, hvis ikke defaulter vi til en immutabel liste
+            ingredientList.getOrDefault(name, Collections.emptyList())
+            // Printer så ut for hver gjenstand i listen, nummerert
+               .forEach(food -> System.out.println((i[0]++) + ". " + food));    
+            // finnes kanskje en lettere måte å gjøre dette på?       
         }
     }
+
+    public void removeIngredient(Ingredient ingredient)
+    {
+        searchIngredient(ingredient);
+        System.out.println("What ingredients do you want to take out of the storage?");
+
+        // må legge til måte å velge mat å ta ut: scanner, split
+    } 
+
 }
