a29076c5022ee926409cc022e5495d6fdaf15aa6
Audun Andreas Langeland Aakermann <audunaakermann@gmail.com>
2024-11-12 18:09:02 +0100

    removeIngredient logic almost complete, needs test

--- foodwaste/src/main/java/edu/ntnu/idi/idatt/FoodStorage.java
+++ foodwaste/src/main/java/edu/ntnu/idi/idatt/FoodStorage.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -17,27 +18,44 @@ public class FoodStorage {
     }
 
     // TODO: add exception-handling
-    public void addIngredient(String name, double price, Date expiryDate, double amount, String unit) {
-        Ingredient ingredient = new Ingredient(name, price, expiryDate, amount, unit);
+    public void addIngredient(Ingredient ingredient) {
+        String name = ingredient.getName();
         ingredientList.putIfAbsent(name, new ArrayList<>()); // looser coupling with arraylist here
         ingredientList.get(name).add(ingredient);
     }
 
     public List<Ingredient> searchIngredient(String name) {
-        {
-            // Check if the name of the ingredient is in the storage. If not, we return an
-            // immutable list.
-            return ingredientList.getOrDefault(name, Collections.emptyList());
+        // Check if the name of the ingredient is in the storage. If not, we return an
+        // immutable list.
+        return ingredientList.getOrDefault(name, Collections.emptyList());
+    }
+
+    public void removeIngredient(String name, double amount) {
+        if (!ingredientList.containsKey(name)) {
+            System.out.println("Item not found.");
+            return;
         }
-    }
 
-    public void removeIngredient(Ingredient ingredient) {        
-        ingredientList.get(ingredient.getName()).remove(ingredient);
-    }
-
-    public void takeOutIngredient(String name, double amount) {
         List<Ingredient> ingredients = ingredientList.get(name);
-        
+        Iterator<Ingredient> iterator = ingredients.iterator();
+
+        while (iterator.hasNext() && amount > 0) {
+            Ingredient ingredient = iterator.next();
+            if (ingredient.getAmount() >= amount) {
+                ingredient.setAmount(ingredient.getAmount() - amount);
+                amount = 0;
+                if (ingredient.getAmount() == 0) {
+                    iterator.remove(); // .remove() should remove it from ingredientList aswell, not 100% sure, needs testing.
+                }
+            }
+            else {
+            amount -= ingredient.getAmount();
+            iterator.remove(); // same as above
+            }
+        }
+        if (amount > 0) {
+            System.out.println("Insufficient amount in storage"); // move this to ui?
+        }
     }
 
     // Map instead of HashMap here to code to an interface directly
