61cbf7695ff5fc2f4e7f414f456cba2f0b98d8ea
Audun Andreas Langeland Aakermann <audunaakermann@gmail.com>
2024-11-12 21:18:01 +0100

    small change to recommendRecipe func

--- foodwaste/src/main/java/edu/ntnu/idi/idatt/model/Cookbook.java
+++ foodwaste/src/main/java/edu/ntnu/idi/idatt/model/Cookbook.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class Cookbook {
     private final List<Recipe> cookbook;
+    private final FoodStorage foodStorage = new FoodStorage();
 
     public Cookbook() {
         cookbook = new ArrayList<>();
@@ -16,7 +17,12 @@ public class Cookbook {
 
     public Recipe recommendRecipe() { // wip needs fixing
         Recipe recommendedRecipe = null;
-
+        for (Recipe recipe : cookbook) {
+            for (Ingredient ingredient : recipe.getIngredients()) {
+                // might be issues with ingredientList being a HashMap with ingredient values? 
+                // TODO: figure out algorithm here, if not need to change from hm to arraylist etc IMPORTANT
+            }
+        }
         return recommendedRecipe;
     }
 }
