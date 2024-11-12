77307373c5486a3494d35d22e0d657d44538bad3
Audun Andreas Langeland Aakermann <audunaakermann@gmail.com>
2024-11-12 20:24:24 +0100

    organized classes according to mvc structure

--- foodwaste/src/main/java/edu/ntnu/idi/idatt/App.java
+++
@@ -1,8 +0,0 @@
-package edu.ntnu.idi.idatt;
-public class App {
-    public static void main(String[] args) throws Exception {
-        UI ui = new UI();
-        ui.init();
-        ui.start();
-    }
-}
