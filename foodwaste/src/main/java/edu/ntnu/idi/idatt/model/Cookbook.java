64db5fd64bf73cc334e4e77a114cc071037cb48a
Audun Andreas Langeland Aakermann <audunaakermann@gmail.com>
2024-11-12 21:11:26 +0100

    finalized all vars

--- foodwaste/src/main/java/edu/ntnu/idi/idatt/model/Cookbook.java
+++ foodwaste/src/main/java/edu/ntnu/idi/idatt/model/Cookbook.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cookbook {
-    private List<Recipe> cookbook;
+    private final List<Recipe> cookbook;
 
     public Cookbook() {
         cookbook = new ArrayList<>();
--- foodwaste/src/main/java/edu/ntnu/idi/idatt/model/Recipe.java
+++ foodwaste/src/main/java/edu/ntnu/idi/idatt/model/Recipe.java
@@ -3,10 +3,10 @@ package edu.ntnu.idi.idatt.model;
 import java.util.List;
 
 public class Recipe {
-    private String name;
-    private String description;
-    private String instruction;
-    private List<Ingredient> ingredients; // possibly not a list of ingredients, come back to this TODO
+    private final String name;
+    private final String description;
+    private final String instruction;
+    private final List<Ingredient> ingredients; // possibly not a list of ingredients, come back to this TODO
 
     public Recipe(String name, String description, String instruction, List<Ingredient> ingredients) {
         this.name = name;
