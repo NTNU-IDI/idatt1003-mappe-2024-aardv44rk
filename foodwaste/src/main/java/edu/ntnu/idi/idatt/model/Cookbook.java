b28031f0da3af306946216706643b65384aff4b4
Audun Andreas Langeland Aakermann <audunaakermann@gmail.com>
2024-11-12 21:09:39 +0100

    added basic functionality

--- foodwaste/src/main/java/edu/ntnu/idi/idatt/model/Cookbook.java
+++ foodwaste/src/main/java/edu/ntnu/idi/idatt/model/Cookbook.java
@@ -1,5 +1,22 @@
 package edu.ntnu.idi.idatt.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cookbook {
-    
+    private List<Recipe> cookbook;
+
+    public Cookbook() {
+        cookbook = new ArrayList<>();
+    }
+
+    public void addRecipe(Recipe recipe) {
+        cookbook.add(recipe);
+    }
+
+    public Recipe recommendRecipe() { // wip needs fixing
+        Recipe recommendedRecipe = null;
+
+        return recommendedRecipe;
+    }
 }
--- foodwaste/src/main/java/edu/ntnu/idi/idatt/model/Recipe.java
+++ foodwaste/src/main/java/edu/ntnu/idi/idatt/model/Recipe.java
@@ -1,5 +1,33 @@
-package edu.ntnu.idi.idatt;
+package edu.ntnu.idi.idatt.model;
+
+import java.util.List;
 
 public class Recipe {
+    private String name;
+    private String description;
+    private String instruction;
+    private List<Ingredient> ingredients; // possibly not a list of ingredients, come back to this TODO
 
+    public Recipe(String name, String description, String instruction, List<Ingredient> ingredients) {
+        this.name = name;
+        this.description = description;
+        this.instruction = instruction;
+        this.ingredients = ingredients;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getInstruction() {
+        return instruction;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
 }
