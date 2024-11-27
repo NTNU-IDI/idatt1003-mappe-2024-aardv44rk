93a6f0ea3b2e4b9794f10143b8f15bbd27808dd6
Audun Andreas Langeland Aakermann <audunaakermann@gmail.com>
2024-11-27 19:50:41 +0100

    added Cookbook and updated ArgumentValidator class for class checks

---
+++ foodwaste/src/main/java/edu/ntnu/idi/idatt/model/Cookbook.java
@@ -0,0 +1,11 @@
+package edu.ntnu.idi.idatt.model;
+
+import java.util.List;
+
+public class Cookbook {
+  private List<Recipe> recipes;
+
+  public Cookbook() {
+    recipes = new ArrayList;
+  }
+}
