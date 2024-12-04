ca5ecfaa0963876185d6bdd2831c372d7adf5e40
Audun Andreas Langeland Aakermann <audunaakermann@gmail.com>
2024-12-04 18:18:12 +0100

    App.java initial commit

---
+++ foodwaste/src/main/java/edu/ntnu/idi/idatt/App.java
@@ -0,0 +1,13 @@
+package edu.ntnu.idi.idatt;
+
+import edu.ntnu.idi.idatt.view.UserInterface;
+
+/**
+ * 
+ */
+public class App {
+  public static void main(String[] args) {
+    UserInterface ui = new UserInterface();
+    ui.init();
+  }
+}
