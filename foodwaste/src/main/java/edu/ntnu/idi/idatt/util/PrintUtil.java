d55f614d2dd6ab5e33d394d9d87b4d6f1ec60fc8
Audun Andreas Langeland Aakermann <audunaakermann@gmail.com>
2024-12-04 20:57:52 +0100

    Added PrintUtil class and methods to print lists and maps.

---
+++ foodwaste/src/main/java/edu/ntnu/idi/idatt/util/PrintUtil.java
@@ -0,0 +1,47 @@
+package edu.ntnu.idi.idatt.util;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+/**
+ * Class responsible for converting objects to strings and printing them.
+ */
+public class PrintUtil {
+  private PrintUtil() {
+    throw new UnsupportedOperationException("Utility class");
+  }
+
+  /**
+   * Prints the objects in a List {@code list} using the function {@code mapper} to convert the
+   * object to a string.
+   *
+   * @param <T> the type of the objects in the list
+   * @param list the list to be printed
+   * @param mapper the function that converts the object to a string
+   */
+  public static <T> void printList(List<T> list, Function<T, String> mapper) {
+    list.stream()
+        .map(mapper)
+        .forEach(System.out::println);
+  }
+
+  /**
+   * Prints the values in a Map {@code map} using the function {@code mapper} to convert the object
+   * to a string. If the values are lists, they are printed using the function
+   * {@link #printList(List, Function)} and {@code mapper}.
+   *
+   * @param <T> the type of the values in the map
+   * @param map the map to be printed
+   * @param mapper the function that converts the object to a string
+   */
+  public static <T> void printMap(Map<String, T> map, Function<T, String> mapper) {
+    map.values().forEach(value -> {
+      if (value instanceof List) {
+        printList((List<T>) value, mapper);
+      } else {
+        System.out.println(mapper.apply(value));
+      }
+    });
+  }
+}
