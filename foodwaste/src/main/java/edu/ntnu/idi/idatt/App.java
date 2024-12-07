e854a113bb7d54b52ec7be5a31f5c64f871f59a6
Audun Andreas Langeland Aakermann <audunaakermann@gmail.com>
2024-12-07 17:09:56 +0100

    added instantiation of objects to main, note: move this to separate function

--- foodwaste/src/main/java/edu/ntnu/idi/idatt/App.java
+++ foodwaste/src/main/java/edu/ntnu/idi/idatt/App.java
@@ -1,13 +1,30 @@
 package edu.ntnu.idi.idatt;
 
-import edu.ntnu.idi.idatt.view.UserInterface;
+import edu.ntnu.idi.idatt.controllers.CookbookController;
+import edu.ntnu.idi.idatt.controllers.StorageController;
+import edu.ntnu.idi.idatt.model.Cookbook;
+import edu.ntnu.idi.idatt.model.FoodStorage;
+import edu.ntnu.idi.idatt.view.MainMenu;
 
 /**
- * 
+ * The main class of the application. Runs the UserInterface, 
+ * which is the main view of the application.
+ *
+ * @author aardv44rk
+ * @since December 4th 2024
+ * @version 2.0
  */
 public class App {
+  /**
+   * The main method of the application.
+   *
+   * @param args the command line arguments
+   */
   public static void main(String[] args) {
-    UserInterface ui = new UserInterface();
-    ui.init();
+    Cookbook cookbook = new Cookbook();
+    FoodStorage storage = new FoodStorage();
+    StorageController storageController = new StorageController(storage);
+    CookbookController cookbookController = new CookbookController(cookbook, storageController);
+    new MainMenu(storageController, cookbookController).display();
   }
 }
