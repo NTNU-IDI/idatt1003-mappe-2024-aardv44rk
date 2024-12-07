377ce7494ba07e3918acf64e0562139215c64236
Audun Andreas Langeland Aakermann <audunaakermann@gmail.com>
2024-12-07 12:19:00 +0100

    fixed inputvalidator, removing loops in methods.

--- foodwaste/src/main/java/edu/ntnu/idi/idatt/util/InputValidator.java
+++ foodwaste/src/main/java/edu/ntnu/idi/idatt/util/InputValidator.java
@@ -6,6 +6,8 @@ import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 import java.util.Scanner;
 
+
+// TODO move loops to UI
 /**
  * Class responsible for validating user inputs by making sure they can be parsed to the correct
  * datatypes.
@@ -16,12 +18,14 @@ import java.util.Scanner;
  */
 public class InputValidator {
 
+  private static final Scanner sc = new Scanner(System.in);
+
   /**
    * Constructor that throws an UnsupportedOperationException to prevent instantiation of the class.
    *
    * @throws UnsupportedOperationException if the class is instantiated
    */
-  protected InputValidator() throws UnsupportedOperationException {
+  private InputValidator() throws UnsupportedOperationException {
     throw new UnsupportedOperationException("Utility class");
   }
 
@@ -29,23 +33,18 @@ public class InputValidator {
    * Prompts the user for a String with prompt {@code p}. 
    * Validates and returns the String if it is not empty.
    *
-   * @param sc Scanner that reads user input
    * @param p Prompt that user receives through the terminal
    * @return The string input of the user if it is validated
    * @throws InvalidInputException if user input is blank
    */
-  public static String getString(Scanner sc, String p) throws InvalidInputException {
+  public static String getString(String p) throws InvalidInputException {
     System.out.println(p);
-    boolean valid = false;
     String output = "";
     String input = sc.nextLine();
-    while (!valid) {
-      if (input.isBlank()) {
-        throw new InvalidInputException("Blank input not allowed, sorry!");
-      } else {
-        valid = true;
-        output = input;
-      }
+    if (input.isBlank()) {
+      throw new InvalidInputException("Blank input not allowed, sorry!");
+    } else {
+      output = input;
     }
     return output;
   }
@@ -54,26 +53,21 @@ public class InputValidator {
    * Prompts the user for a String with prompt {@code p}.
    * Validates and returns if it can be parsed to a double.
    *
-   * @param sc Scanner that reads user input
    * @param p Prompt that user receives 
    * @return The string input of the user if it is validated
    * @throws InvalidInputException if user input is blank
    */
-  public static double getDouble(Scanner sc, String p) throws InvalidInputException {
+  public static double getDouble(String p) throws InvalidInputException {
     System.out.println(p);
-    boolean valid = false;
     double output = 0;
     String input = sc.nextLine();
-    while (!valid) {
-      if (input.isBlank()) {
-        throw new InvalidInputException("Blank input not allowed, sorry!");
-      } else {
-        try {
-          output = Double.parseDouble(input);
-          valid = true;
-        } catch (NumberFormatException e) {
-          System.out.println("Input has to be a number, whoops!");
-        }
+    if (input.isBlank()) {
+      throw new InvalidInputException("Blank input not allowed, sorry!");
+    } else {
+      try {
+        output = Double.parseDouble(input);
+      } catch (NumberFormatException e) {
+        System.out.println("Input has to be a number, whoops!");
       }
     }
     return output;
@@ -83,28 +77,23 @@ public class InputValidator {
    * Prompts the user for a String on a certain format with prompt {@code p}.
    * Validates and returns the String if it is successfully parsed to {@link java.time.LocalDate}.
    *
-   * @param sc Scanner that reads user input
    * @param p Prompt that user receives 
    * @return The string input of the user if it is validated
    * @throws IllegalArgumentException if user input does not match the required date format
    */
-  public static LocalDate getDate(Scanner sc, String p) throws UnsupportedFormatException {
+  public static LocalDate getDate(String p) throws UnsupportedFormatException {
     System.out.println(p);
-    boolean valid = false;
     LocalDate output = LocalDate.MIN;
     String input = sc.nextLine();
-    while (!valid) {
-      if (input.isBlank()) {
-        throw new IllegalArgumentException("Blank input not allowed, sorry!");
-      } else {
-        try {
-          output = DateUtil.parseDate(input);
-          valid = true;
-        } catch (DateTimeParseException e) {
-          System.out.println("Input has to be a date in the format dd-MM-yyyy, whoops!");
-        }
+    if (input.isBlank()) {
+      throw new IllegalArgumentException("Blank input not allowed, sorry!");
+    } else {
+      try {
+        output = DateUtil.parseDate(input);
+      } catch (DateTimeParseException e) {
+        System.out.println("Input has to be a date on the format dd-MM-yyyy, whoops!");
       }
-    }      
+    }
     return output;
   }
 }
