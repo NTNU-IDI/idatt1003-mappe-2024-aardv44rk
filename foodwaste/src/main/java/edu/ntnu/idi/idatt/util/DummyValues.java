a9a98e271288162e2f744b76eb5f4b4fcae95516
Audun Andreas Langeland Aakermann <audunaakermann@gmail.com>
2024-12-10 16:44:19 +0100

    Small changes in DummyValues class

--- foodwaste/src/main/java/edu/ntnu/idi/idatt/util/DummyValues.java
+++ foodwaste/src/main/java/edu/ntnu/idi/idatt/util/DummyValues.java
@@ -15,25 +15,22 @@ public class DummyValues {
   private DummyValues() {
     throw new UnsupportedOperationException("Utility class");
   }
+
   /**
    * Initializes dummy values for testing purposes.
    *
-   * @param storageController the storage controller
+   * @param storageController  the storage controller
    * @param cookbookController the cookbook controller
    */
   public static void initializeDummyValues(
-        StorageController storageController,
-        CookbookController cookbookController
-  ) {
+      StorageController storageController,
+      CookbookController cookbookController) {
     storageController.addIngredient(
-      new Ingredient("Egg", 20, DateUtil.parseDate("12-12-2024"), 6, "pcs")
-    );
+        new Ingredient("Egg", 20, DateUtil.parseDate("12-12-2024"), 6, "pcs"));
     storageController.addIngredient(
-      new Ingredient("Milk", 1000, DateUtil.parseDate("20-12-2024"), 1000, "ml")
-    );
+        new Ingredient("Milk", 15, DateUtil.parseDate("20-12-2024"), 1000, "ml"));
     storageController.addIngredient(
-      new Ingredient("Flour", 500, DateUtil.parseDate("12-06-2025"), 1000, "g")
-    );
+        new Ingredient("Flour", 15, DateUtil.parseDate("12-06-2025"), 1000, "g"));
 
     Map<String, Ingredient> ingredients = new LowerCaseMap<>();
     ingredients.put("Egg", new Ingredient("Egg", 6, "pcs"));
@@ -41,7 +38,7 @@ public class DummyValues {
     ingredients.put("Flour", new Ingredient("Flour", 500, "g"));
 
     cookbookController.addRecipe(
-      new Recipe("Pancakes", "Delicious panckas", "Mix the ingredients, serve", 4.0, ingredients)
+        new Recipe("Pancakes", "Delicious panckas", "Mix the ingredients, serve", 4.0, ingredients)
     );
   }
 }
